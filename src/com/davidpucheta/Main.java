b3cc4b0b313b97de08174f3f6da70f979bbbcc09
David Reyes-Pucheta <davidpucheta@gmail.com>
2020-01-07 10:54:14 -0600

    Polymorphism example

--- src/com/davidpucheta/Main.java
+++ src/com/davidpucheta/Main.java
@@ -21,13 +21,20 @@ public class Main {
 		var mailService = new MailService();
 		mailService.sendEmail();
 
-		//Inheritance
+		//Inheritance 3rd principle of OOP
 		var textBox = new TextBox();
 		textBox.enable();
 
+		//Polymorphism - 4th principle of OOP
+		drawUIControl(new CheckBox());
+		drawUIControl(new TextBox());
     }
 
     public static TaxCalculator getCalculator() {
     	return new TaxCalculator2019();
+	}
+
+	public static void drawUIControl(UIControl control){
+		control.draw();
 	}
 }
--- src/com/davidpucheta/TextBox.java
+++ src/com/davidpucheta/TextBox.java
@@ -1,4 +1,8 @@
 package com.davidpucheta;
 
 public class TextBox extends UIControl {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a textbox");
+    }
 }
